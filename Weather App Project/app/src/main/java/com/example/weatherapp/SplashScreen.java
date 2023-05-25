83c9686afc22de05d524ba3610a43a497c8604a3
sany5881 <125036439+sany5881@users.noreply.github.com>
2023-05-26 00:37:51 +0500

    Upload project

---
+++ Weather App Project/app/src/main/java/com/example/weatherapp/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.weatherapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.weatherapp.databinding.ActivitySplashScreenBinding;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+    private ActivitySplashScreenBinding binding;
+    private final int SPLASH_TIME = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        splashScreen();
+    }
+
+    private void splashScreen() {
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_TIME);
+    }
+}
---
+++ Weather App Project/app/src/main/java/com/example/weatherapp/location/LocationCord.java
@@ -0,0 +1,9 @@
+package com.example.weatherapp.location;
+
+public class LocationCord {
+    public static String lat = "";
+    public static String lon = "";
+    //    public final static String API_KEY = "0bc14881636d2234e8c17736a470535f";eeb8b40367eee691683e5a079e2fa695
+    public final static String API_KEY = "0bc14881636d2234e8c17736a470535f";
+}
+
